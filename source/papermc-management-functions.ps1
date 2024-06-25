51fbf41ba5ed108b377bee279d9d65eef37b5a3b
R. L. Vandaveer <rlvandaveer@heliar.io>
2024-06-24 21:53:47 -0400

    Add new server creation cmdlet and minor updates
    - Add New-PaperServer cmdlet
    - Add/improve help text
    - Update default Minecraft version targets

--- source/papermc-management-functions.ps1
+++ source/papermc-management-functions.ps1
@@ -2,7 +2,7 @@
 .SYNOPSIS
 	Downloads the latest Paper build and places it into the desired server paths
 .DESCRIPTION
-	This Cmdlet can be used to update the Paper mod in one or more server directories.
+	This Cmdlet can be used to update the Paper server JAR file in one or more server directories.
 .PARAMETER PaperDownloadUri
 	Overrides the default download Uri for Paper
 .PARAMETER MinecraftVersion
@@ -18,7 +18,7 @@ function Update-PaperVersion {
 		[Uri]$PaperDownloadUri,
 		[Parameter(Mandatory = $false)]
 		[ValidateNotNullOrEmpty()]
-		[string]$MinecraftVersion = '1.19.3',
+		[string]$MinecraftVersion = '1.21',
 		[Parameter(Mandatory = $true, ValueFromPipeline = $true)]
 		[ValidateScript({
 			if (-not ($_ | Test-Path)) { throw 'Source is not a valid path'}
@@ -107,12 +107,26 @@ function Update-PaperVersion {
 	}
 }
 
+<#
+.SYNOPSIS
+	Updates the start-up script in the server path(s) to the latest paper version.
+.DESCRIPTION
+	This commandlet finds the previous minecraft version in the start-up script and replaces it with the new version.
+.PARAMETER MinecraftVersion
+	The version of Minecraft to target
+.PARAMETER PreviousPaperJar
+	The previous Paper JAR file
+.PARAMETER NewPaperBuild
+	The new Paper build number
+.PARAMETER ServerPath
+	The path(s) to the server directories to update
+#>
 function Update-StartScript {
 	[CmdletBinding()]
 	param (
 		[Parameter(Mandatory = $false)]
 		[ValidateNotNullOrEmpty()]
-		[string]$MinecraftVersion = '1.19.3',
+		[string]$MinecraftVersion = '1.21',
 		[Parameter(Mandatory = $true)]
 		[ValidateNotNullOrEmpty()]
 		$PreviousPaperJar,
@@ -145,4 +159,125 @@ function Update-StartScript {
 	end {
 
 	}
+}
+
+<#
+.SYNOPSIS
+	This commandlet will create a new Paper server with default files
+.DESCRIPTION
+
+.PARAMETER PaperDownloadUri
+	The URL where to download the Paper files. This only needs to be set if overriding the default location
+.PARAMETER MinecraftVersion
+	The Minecraft version to target
+.PARAMETER WorldName
+	The name of the new server world
+.PARAMETER ServerPath
+	The path(s) to the server directory(ies)
+#>
+function New-PaperServer {
+	[CmdletBinding(SupportsShouldProcess = $true)]
+	param (
+		[Parameter(Mandatory = $false)]
+		[ValidateNotNullOrEmpty()]
+		[Uri]$PaperDownloadUri,
+		[Parameter(Mandatory = $false)]
+		[ValidateNotNullOrEmpty()]
+		[string]$MinecraftVersion = '1.21',
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[string]$WorldName,
+		[Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+		[ValidateScript({
+			if ($_ | Test-Path) { throw 'Source is an existing path'}
+			if (-not ($_ | Test-Path -IsValid)) { throw 'Source is not a valid path syntax'}
+			$true
+		})]
+		[string]$ServerPath
+	)
+
+	begin {
+
+		if ([String]::IsNullOrWhiteSpace($PaperDownloadUri)) {
+
+			New-Variable -Name VERSION_INFO_URI -Value "https://api.papermc.io/v2/projects/paper/versions/$MinecraftVersion/" -Option Constant -WhatIf:$false
+			Write-Verbose "Retrieving Paper builds for Minecraft version $MinecraftVersion..."
+			$versionInfo = Invoke-RestMethod -Uri $VERSION_INFO_URI -Method Get -StatusCodeVariable versionInfoResponseCode
+
+			if ($null -eq $versionInfo) {
+				throw 'Could not retrieve version information for Paper'
+			}
+
+			$buildNumber = ($versionInfo.builds | Measure-Object -Maximum).Maximum
+			Write-Verbose "Latest Paper build version number for $MinecraftVersion is $buildNumber"
+			$fileUri = "https://api.papermc.io/v2/projects/paper/versions/$MinecraftVersion/builds/$buildNumber/downloads/paper-$MinecraftVersion-$buildNumber.jar"
+			$fileName = Split-Path -Path $fileUri -Leaf
+			$tempFile = (Join-Path -Path ([System.IO.Path]::GetTempPath()) -ChildPath $fileName)
+
+			if ($PSCmdlet.ShouldProcess($fileUri, "Download Paper build")) {
+
+				Write-Verbose "Downloading $fileName to $tempFile..."
+				$response = Invoke-WebRequest -Uri $fileUri -OutFile $tempFile
+
+			}
+
+		} else {
+
+			$fileName = Split-Path -Path $PaperDownloadUri -Leaf
+			$tempFile = (Join-Path -Path ([System.IO.Path]::GetTempPath()) -ChildPath $fileName)
+
+			if ($PSCmdlet.ShouldProcess($fileUri, "Download Paper build")) {
+
+				Write-Verbose "Downloading $fileName to $tempFile..."
+				$response = Invoke-WebRequest -Uri $PaperDownloadUri -OutFile $tempFile
+
+			}
+
+		}
+
+	}
+
+	process {
+
+		if ($PSCmdlet.ShouldProcess($ServerPath, "Create server game directory")) {
+
+			Write-Verbose "Creating $ServerPath..."
+			New-Item -Path $ServerPath -ItemType Directory
+
+		}
+
+		if ($PSCmdlet.ShouldProcess($ServerPath, "Create base files")) {
+
+			Write-Verbose "Cloning base server directory..."
+			git clone --depth=1 git@github.com:rlvandaveer/hangfires-papermc-base-server.git $ServerPath
+			Remove-Item -Path (Join-Path -Path $ServerPath -ChildPath '.git') -Recurse -Force
+
+			New-Variable -Name SCRIPT_NAME -Value "start.ps1" -Option Constant
+			Write-Verbose "Preparing startup script $SCRIPT_NAME..."
+
+			New-Variable -Name TEMPLATE_VALUE -Value '{{paper.jar}}' -Option Constant
+			Write-Verbose "Rename startup template file $TEMPLATE_VALUE..."
+			$scriptPath = (Join-Path -Path $ServerPath -ChildPath $SCRIPT_NAME)
+			Rename-Item -Path (Join-Path -Path $ServerPath -ChildPath start-template.ps1) -NewName $scriptPath
+
+			Write-Verbose "Updating $SCRIPT_NAME to execute $fileName..."
+			(Get-Content -Path $scriptPath) -replace $TEMPLATE_VALUE, $fileName | Set-Content $scriptPath
+
+		}
+
+		if ($PSCmdlet.ShouldProcess($ServerPath, "Copy Paper build")) {
+
+			$destination = Join-Path -Path $ServerPath -ChildPath $fileName
+			Write-Verbose "Copying $fileName to $destination..."
+			Copy-Item -Path $tempFile -Destination $destination
+
+		}
+
+	}
+
+	end {
+
+
+	}
+
 }
